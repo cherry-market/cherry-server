9bc294adc73b22f418363cd8dd282f0053d3afed
Juferis <66511872+Juferis@users.noreply.github.com>
2026-01-21 00:04:31 +0900

    feat: implement health check api

---
+++ src/main/java/com/cherry/server/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.cherry.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Cherry Server is up and running!";
+    }
+}
